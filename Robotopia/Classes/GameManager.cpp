03cbe28800c340229a19039faa06e853fa21c83d
jwvg0425 <jwvg0425@naver.com>
2014-10-26 13:34:48 +0900

    ManagerBase
    ManagerMase

---
+++ Robotopia/Classes/GameManager.cpp
@@ -0,0 +1,33 @@
+﻿#include "GameManager.h"
+
+GameManager* GameManager::m_Instance = nullptr;
+
+
+GameManager* GameManager::getInstance()
+{
+	if (m_Instance == nullptr)
+	{
+		m_Instance = new GameManager();
+	}
+	return m_Instance;
+}
+
+void GameManager::releaseInstance()
+{
+	if (m_Instance != nullptr)
+	{
+		delete m_Instance;
+		m_Instance = nullptr;
+	}
+}
+
+GameManager::GameManager()
+{
+
+}
+
+GameManager::~GameManager()
+{
+
+}
+
---
+++ Robotopia/Classes/GameManager.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#define GET_GAME_MANAEGR() GameManager::getInstance()
+
+class GameManager
+{
+public:
+	GameManager* getInstance();
+	void releaseInstance();
+private:
+	static GameManager* m_Instance;
+	GameManager();
+	~GameManager();
+};
