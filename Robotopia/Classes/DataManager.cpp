5204ec2690701296c2a60f63762887aab41a9ecf
jwvg0425 <jwvg0425@naver.com>
2014-10-29 21:34:14 +0900

    moduleData road&save
    moduleData road&save

--- Robotopia/Classes/DataManager.cpp
+++ Robotopia/Classes/DataManager.cpp
@@ -18,42 +18,151 @@ bool Arthas::DataManager::init()
 	{
 		m_ModuleDatas[dirType].reserve(40);
 	}
-	Arthas::ModuleData data;
 
-	data.width = 20;
-	m_ModuleDatas[0].push_back(data);
-
-	saveModuleData();
 	return true;
 }
 
 bool Arthas::DataManager::loadModuleData()
 {
+	ssize_t bufferSize = 0;
+	unsigned char* fileData = cocos2d::FileUtils::getInstance()->getFileData(MODULE_FILE_NAME, "rb", &bufferSize);
+	std::string clearData((const char*)fileData, bufferSize);
+
+	Json::Value root;
+	Json::Reader reader;
+	char key[BUF_SIZE] = {};
+	bool isParsingSuccess = reader.parse(clearData, root);
+
+	if (!isParsingSuccess)
+	{
+		cocos2d::log("parser failed : \n %s", MODULE_FILE_NAME);
+		return false;
+	}
+
+	for (int dirType = 0; dirType < DIR_MAX; dirType++)
+	{
+		int size;
+
+		m_ModuleDatas[dirType].clear();
+
+		ModuleData data;
+
+		getModuleKey(dirType, "size", key);
+		size = root.get(key, 0).asInt();
+
+		if (size > 0)
+		{
+
+			for (int idx = 0; idx < size; idx++)
+			{
+				int width, height;
+
+				getModuleKey(dirType, idx, "width", key);
+				width = root.get(key, 0).asInt();
+				getModuleKey(dirType, idx, "height", key);
+				height = root.get(key, 0).asInt();
+
+				data.width = width;
+				data.height = height;
+
+				getModuleKey(dirType, idx, "data", key);
+				Json::Value array = root[key];
+
+				for (int i = 0; i < width * height; i++)
+				{
+					ObjectType type;
+
+					type = (ObjectType)array[i].asInt();
+
+					data.data.push_back(type);
+				}
+			}
+			m_ModuleDatas[dirType].push_back(data);
+		}
+	}
+
 	return true;
 }
 
 
 bool Arthas::DataManager::saveModuleData()
 {
-	Json::Value root;
+	Json::Value moduleData;
+	char buffer[BUF_SIZE] = {};
 
-	root["Name"] = "HongGildong";
-	root["Age"] = 26;
+	for (int dirType = 0; dirType < DIR_MAX; dirType++)
+	{
+		//현재 타입의 모듈 사이즈 삽입
+		getModuleKey(dirType, "size", buffer);
+		moduleData[buffer] = m_ModuleDatas[dirType].size();
 
-	Json::Value friends;
+		for (int idx = 0; idx < m_ModuleDatas[dirType].size(); idx++)
+		{
+			int width, height;
 
-	friends.append("Im kkeokjung");
-	friends.append("Elisabeth");
-	root["Friend"] = friends;
-	root["Sex"] = "male";
+			width = m_ModuleDatas[dirType][idx].width;
+			height = m_ModuleDatas[dirType][idx].height;
+
+			//너비, 높이 삽입
+			getModuleKey(dirType, idx, "width", buffer);
+			moduleData[buffer] = width;
+			getModuleKey(dirType, idx, "height", buffer);
+			moduleData[buffer] = height;
+
+			Json::Value data;
+
+			//세부 데이터(오브젝트 타입들) 삽입
+			for (int i = 0; i < width * height; i++)
+			{
+				data.append(m_ModuleDatas[dirType][idx].data[i]);
+			}
+			getModuleKey(dirType, idx, "data", buffer);
+			moduleData[buffer] = data;
+		}
+	}
 
 	Json::StyledWriter writer;
-	std::string strJSON = writer.write(root);
+	std::string strJSON = writer.write(moduleData);
 
-	cocos2d::log("JSON WriteTest : %s", strJSON.c_str());
-
-	//Json::writeFileData("testJSON.json", strJSON.c_str());
+	saveData(MODULE_FILE_NAME, strJSON.c_str());
 
 	return true;
 }
 
+bool Arthas::DataManager::saveData(std::string fileName, const char* pData)
+{
+	FILE *fp = fopen(fileName.c_str(), "wb");
+
+	if (!fp)
+	{
+		cocos2d::log("can not create file %s", fileName);
+		return false;
+	}
+
+
+	fputs(pData, fp);
+
+	fclose(fp);
+
+	return true;
+}
+
+bool Arthas::DataManager::getModuleKey(int type, int idx, char* category, OUT char* key)
+{
+	if (key == nullptr || category == nullptr)
+		return false;
+
+	sprintf(key, "%d_%d_%s", type, idx, category);
+
+	return true;
+}
+
+bool Arthas::DataManager::getModuleKey(int type, char* category, OUT char* key)
+{
+	if (key == nullptr || category == nullptr)
+		return false;
+
+	sprintf(key, "%d_%s", type, category);
+
+	return true;
+}
--- Robotopia/Classes/DataManager.h
+++ Robotopia/Classes/DataManager.h
@@ -1,12 +1,15 @@
-﻿//
-// DataManager.h
-//
-// 작성자 : 남현욱
-//
-//
-// 외부 파일에서 Data를 가져오는 작업 및 외부 파일에 Data를 쓰는 것과 관련된 작업을 담당한다.
-//
-//
+﻿/************************************************************************/
+/*
+	CLASS			: GameManager
+	Author			: 남현욱
+	역할				: 외부 파일에서 Data를 가져오는 작업 및 외부 파일에 Data를 쓰는 것과 관련된 작업을 담당한다.
+	최종 수정일자	: 2014-10-29
+	최종 수정자		: 
+	최종 수정사유	:
+	Comment			:
+*/
+/************************************************************************/
+
 #pragma once
 #include "Util.h"
 #include "DataType.h"
@@ -24,8 +27,13 @@ public:
 
 	bool						loadModuleData();
 	bool						saveModuleData();
+	
 
 private:
+
+	bool						saveData(std::string str, const char* pData);
+	bool						getModuleKey(int type, int idx, char* category, OUT char* key);
+	bool						getModuleKey(int type, char* category, OUT char* key);
 
 	std::vector<ModuleData>		m_ModuleDatas[DIR_MAX];
 };
