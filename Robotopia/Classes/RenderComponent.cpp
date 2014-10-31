9fe691030f8d8a061d9f1cac1db03b0463686ee2
Jaewoo Woo <gewoose@gmail.com>
2014-10-31 15:52:24 +0900

    rename

---
+++ Robotopia/Classes/RenderComponent.cpp
@@ -0,0 +1,35 @@
+#include "RenderComponent.h"
+#include "Trigger.h"
+#include "StateComponent.h"
+
+
+
+bool Arthas::RenderComponent::init()
+{
+
+}
+
+void Arthas::RenderComponent::enter()
+{
+
+}
+
+void Arthas::RenderComponent::exit()
+{
+
+}
+
+void Arthas::RenderComponent::update(int dTime)
+{
+
+}
+
+void Arthas::RenderComponent::addTransition(Transition addTransition)
+{
+
+}
+
+void Arthas::RenderComponent::removeTransition(Transition remTranstion)
+{
+
+}
--- Robotopia/Classes/ResourceManager.h
+++ Robotopia/Classes/ResourceManager.h
@@ -6,7 +6,7 @@ Author				: 우재우
 최종 수정일자		: 2014-10-31
 최종 수정자			: 우재우
 최종 수정사유		: 리소스 매니저 생성
-Comment				: 아직 미완성이염~
+Comment				: 리소스 재생성 문제(맵 사용) 그대로 둘 것인지 결정 못 했음. 
 */
 /************************************************************************/
 
@@ -27,8 +27,8 @@ public:
 	cocos2d::Sprite*		createSprite(ResourceType type);
 
 private:
-	std::map <ResourceType, cocos2d::Sprite*>		m_Sprites;
-	std::map <ResourceType, cocos2d::Animation*>	m_Animations;
+// 	std::map <ResourceType, cocos2d::Sprite*>		m_Sprites;
+// 	std::map <ResourceType, cocos2d::Animation*>	m_Animations;
 	
 };
 
