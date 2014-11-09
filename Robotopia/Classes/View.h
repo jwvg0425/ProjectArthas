cfa196c79261c97b1c8fba856113de17ed5c29ad
abiles <pgalpga@hanmail.net>
2014-11-09 12:31:04 +0900

    view a little

---
+++ Robotopia/Classes/View.h
@@ -0,0 +1,29 @@
+/************************************************************************/
+/*
+CLASS			: View
+Author			: 김성연
+역할				: State들의 추상 클래스
+최종 수정일자		: 2014-11-09
+최종 수정자		:
+최종 수정사유		:
+Comment			:
+*/
+/************************************************************************/
+
+#pragma once
+#include "pch.h"
+#include "Util.h"
+
+BEGIN_NS_AT
+
+class View
+{
+public:
+	static void initView(cocos2d::Layer* scrollingLayer);
+	static void setViewPort(cocos2d::Point pivotPoint);
+
+private:
+	static cocos2d::extension::ScrollView* m_Scroll;
+};
+
+END_NS_AT
