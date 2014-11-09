7dfebec386bfe8b856bbfaa8e31f87b6a8ee8371
abiles <pgalpga@hanmail.net>
2014-11-09 14:49:14 +0900

    ildan update view

--- Robotopia/Classes/View.cpp
+++ Robotopia/Classes/View.cpp
@@ -1,12 +1,8 @@
-#include "View.h"
 #include "pch.h"
-#include "cocos-ext.h"
-
-#define WINSIZE_WIDTH 960
-#define WINSIZE_HEIGHT 540
+#include "View.h"
 
 
-bool Arthas::View::init()
+bool View::init()
 {
 	if (!cocos2d::extension::ScrollView::init())
 	{
@@ -16,18 +12,18 @@ bool Arthas::View::init()
 	return true;
 }
 
-void Arthas::View::initView(cocos2d::Layer* scrollingLayer)
+void View::initScroll(cocos2d::Layer* scrollingLayer)
 {
-	m_Scroll = cocos2d::extension::ScrollView::
-		create(cocos2d::Size(WINSIZE_WIDTH, WINSIZE_HEIGHT), scrollingLayer);
-
+	m_Scroll = cocos2d::extension::ScrollView::create(cocos2d::Director::getInstance()->getWinSize(),
+													  scrollingLayer);
 	m_Scroll->setDirection(cocos2d::extension::ScrollView::Direction::BOTH);
 	m_Scroll->setBounceable(false);
+	this->addChild(m_Scroll);
+
 }
 
-void Arthas::View::setViewPort(cocos2d::Point pivotPoint)
+void View::setViewPort(cocos2d::Point pivotPoint)
 {
 	m_Scroll->setContentOffset(pivotPoint);
 }
-
 
--- Robotopia/Classes/View.h
+++ Robotopia/Classes/View.h
@@ -2,7 +2,7 @@
 /*
 CLASS			: View
 Author			: 김성연
-역할				: 
+역할				: 플레이어 위치에 따른 뷰 
 최종 수정일자		: 2014-11-09
 최종 수정자		:
 최종 수정사유		:
@@ -13,21 +13,18 @@ Comment			:
 #pragma once
 
 #include "Util.h"
-
-BEGIN_NS_AT
+#include "cocos-ext.h"
+#include "cocos2d.h"
 
 class View : public cocos2d::extension::ScrollView
 {
 public:
 	OVERRIDE bool init();
-	//인자로 뷰의 대상이 되는 레이어를 넣으시오
-	void		  initView(cocos2d::Layer* scrollingLayer);
-	//인자로 현재 플레이어 위치값을 넣으시오
+	void		  initScroll(cocos2d::Layer* scrollingLayer);
 	void		  setViewPort(cocos2d::Point pivotPoint);
 
 	CREATE_FUNC(View);
+
 private:
 	cocos2d::extension::ScrollView* m_Scroll;
 };
-
-END_NS_AT
