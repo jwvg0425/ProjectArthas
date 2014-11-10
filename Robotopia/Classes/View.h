4087e795a5956c297d6b237471e55c822bdc366a
abiles <pgalpga@hanmail.net>
2014-11-10 16:51:17 +0900

    view

--- Robotopia/Classes/View.h
+++ Robotopia/Classes/View.h
@@ -18,8 +18,9 @@ BEGIN_NS_AT
 
 class View
 {
+public:
 	// standardPoint로 기준점이 되는 점을 넘겨라, anchorPoint(0~1)
-	static void setViewPort(cocos2d::Layer* layer, cocos2d::Point standardPoint, cocos2d::Point anchorPoint);
+	static void setViewPort(cocos2d::Layer* layer, cocos2d::Point playerPosInRoomLayer, cocos2d::Point anchorPoint);
 
 	// Scene 중에 원하는 위치와 Width Height를 넘긴다 그러면 넘긴 위치를 중심으로 잘라낸 크기만큼의 
 	//그림이 윈도우 창을 꽉차게 만든다
