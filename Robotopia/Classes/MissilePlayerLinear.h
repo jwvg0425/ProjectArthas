c1e6c73fae0907c23506b59e17328e281934999d
abiles <pgalpga@hanmail.net>
2014-11-15 14:00:46 +0900

    playerLinearMissile 업데이트 부분만 빼고 구축

---
+++ Robotopia/Classes/MissilePlayerLinear.h
@@ -0,0 +1,46 @@
+/************************************************************************/
+/*
+CLASS			: MissilePlayerLinear
+Author			: 김성연
+역할				: Player의 Linear Missile에 관한 내용 가지고 있음
+최종 수정일자		: 2014-11-15
+최종 수정자		:
+최종 수정사유		:
+Comment			:
+*/
+/************************************************************************/
+#pragma once
+
+#include "Missile.h"
+#include "Util.h"
+
+BEGIN_NS_AT
+
+class MissilePlayerLinear : public Arthas::Missile
+{
+public:
+	OVERRIDE bool				init();
+	OVERRIDE void				update(float dTime);
+	OVERRIDE void				enter();
+	OVERRIDE void				exit();
+
+	OVERRIDE void			    initMissile();
+	OVERRIDE void				setAttribute(cocos2d::Point pos, Direction attackDir = DIR_NONE,
+											 float damage = 0,
+											 cocos2d::Size contentsSize = cocos2d::Size::ZERO,
+											 cocos2d::Vec2 velocity = cocos2d::Point::ZERO,
+											 cocos2d::Point targetPos = cocos2d::Point::ZERO);
+
+
+	CREATE_FUNC(MissilePlayerLinear);
+
+private:
+	int				m_MovedDistance;
+	cocos2d::Point  m_StartPos;
+	cocos2d::Point  m_CurPos;
+	bool			m_IsStartPosCheck;
+
+
+};
+
+END_NS_AT
