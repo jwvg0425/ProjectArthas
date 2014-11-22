f75048e4a327b09af21b035bef1ead4902eaa439
abiles <pgalpga@hanmail.net>
2014-11-22 17:08:05 +0900

    몬스터 스탠드 헤더 추가

---
+++ Robotopia/Classes/MonsterStand.h
@@ -0,0 +1,64 @@
+/************************************************************************/
+/*
+CLASS			: MonsterStand
+Author			: 김성연
+역할				: 서서 공격하는 몬스터 클래스 
+최종 수정일자		: 2014-11-22
+최종 수정자		:
+최종 수정사유		:
+Comment			: 
+*/
+/************************************************************************/
+#pragma once
+#include "Thing.h"
+#include "Util.h"
+
+class MonsterStand : public Thing
+{
+public:
+	enum State
+	{
+		STAT_IDLE,
+		STAT_ATTACK,
+		STAT_NUM,
+	};
+
+	struct MonsterStandInfo
+	{
+		float				coolTime;
+		int					repeatAttackNum;
+		Direction			dir;
+		int					damage;
+		int					maxHp;
+		int					curHp;
+		ComponentType		missileType;
+	};
+
+	OVERRIDE bool			init();
+	OVERRIDE void			update(float dTime);
+	OVERRIDE void			enter();
+	OVERRIDE void			exit();
+
+	CREATE_FUNC(MonsterStand);
+
+	bool						onContactBegin(cocos2d::PhysicsContact& contact);
+	void						onContactSeparate(cocos2d::PhysicsContact& contact);
+	cocos2d::PhysicsBody*		getBody();
+
+	//FSM
+	static void				attack(Thing* target, double dTime, int idx);
+	static void				enterAttack(Thing* target, double dTime, int idx);
+	static void				exitAttack(Thing* target, double dTime, int idx);
+
+	static void				idleTransition(Thing* target, double dTime, int idx);
+	static void				attackTransition(Thing* target, double dTime, int idx);
+	MonsterStandInfo&		getInfo();
+	void					setInfo(float coolTime = 5, int repeatAttackNum = 2, Direction dir = DIR_RIGHT,
+									int damage = 20, int maxHp = 100, int curHp = 100, 
+									ComponentType missileType = OT_MISSILE_PLAYER_LINEAR );
+
+private:
+	cocos2d::PhysicsBody*		m_Body = nullptr;
+	MonsterStandInfo			m_Info;
+	int							m_CurAttackNum;
+};
