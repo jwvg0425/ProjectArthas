7f9419d77671cea166d821795bfb7a5ff3d66cb3
abiles <pgalpga@hanmail.net>
2014-12-03 19:44:21 +0900

    equipmentStatuslayer 클릭시 정보 바뀌게

--- Robotopia/Classes/EquipmentStatusLayer.h
+++ Robotopia/Classes/EquipmentStatusLayer.h
@@ -13,26 +13,63 @@ Comment			:
 #pragma once
 #include "Util.h"
 #include "AssemblyUILayer.h"
+#include "EquipmentAbstract.h"
 
+
+struct AllStatus
+{
+	//head
+	float		m_CoolDown = 0.f;
+	float		m_Mainmemory = 0.f;
+	//engine
+	float		m_ElectronicPower = 0.f;
+	float       m_SteamEffectiveness = 0.f;
+	//armor
+	float       m_DefensivePower = 0.f;
+	float		m_Resistance = 0.f;
+	//steamcontainer
+	float       m_MaxSteam = 0.f;
+	float		m_AbsorbEffectiveness = 0.f;
+	//melee
+	float		m_MeleeDamage = 0.f;
+	float       m_MeleeAttackSpeed = 0.f;
+	//range
+	float       m_RangeDamage = 0.f;
+	float		m_RangeAttackSpeed = 0.f;
+	float		m_AttackRange = 0.f;
+	//leg
+	float		m_Speed = 0.f;
+	float		m_Jump = 0.f;
+
+};
+
+enum AllstatusEnum
+{
+	STATUS_ENUM_START = 0,
+	COOLDOWN = STATUS_ENUM_START,
+	MAINMEMORY,
+	POWER,
+	STEAM_EFF,
+	ARMOR,
+	RESISTANCE,
+	MELEE_DAMAGE,
+	MELEE_ATTACK_SPEED,
+	RANGE_DAMAGE,
+	RANGE_ATTACK_SPEED,
+	RANGE_ATTACK_RANGE,
+	MAX_STEAM,
+	ABSORB_EFF,
+	SPEED,
+	JUMP,
+	STATUS_ENUM_END,
+
+};
 
 class ButtonLayer;
 
 class EquipmentStatusLayer : public AssemblyUILayer
 {
 public:
-
-
-	struct Equipmentes
-	{
-		HeadList		   m_CurChosenHead = HL_START;
-		ArmorList		   m_CurChosenArmor = AL_START;
-		EngineList		   m_CurChosenEngine = EL_START;
-		LegList			   m_CurChosenLeg = LL_START;
-		MeleeList		   m_CurChosenMelee = ML_START;
-		RangeList		   m_CurChosenRangeList = RL_START;
-		SteamContainerList m_CurChosenSteamContainerList = SCL_START;
-
-	};
 
 	enum BasicStatus
 	{
@@ -60,42 +97,55 @@ public:
 
 	CREATE_FUNC(EquipmentStatusLayer);
 
-	void					setStartEquipmentType(Equipmentes equipmentList);
-	void					setBasicLabels();
 	void					setButtons();
+	void					setBasicLabels();
 	void					setAllStatusDescLabels();
 	void					setAllStatusValueLabels();
-
+	void					setAllStatusChangeValueLabels();
+	void					makeArrowLabels();
 
 	//라벨 배치
 	void					setPosBasicDescLabel();
 	void					setPosBasicValueLabel();
 	void					setPosAllStatusDescLabel();
 	void					setPosAllStatusValueLabel();
+	void					setPosAllStatusChangeValueLabel();
 
 	//update시 변경 사항들
-	void					changeBasicItemValue();
+	void					upgradeButtonClick();
+	
+	void					clickedSomeEquipment();
+	void					changeBasicItemValue(const EquipmentInfo* equipmentInfo);
+	void					calculateChangeValue(const EquipmentInfo* equipInfo, EquipmentType equipType);
+	void					updateChangeValueLabel(EquipmentType equipType);
+
+	//버튼 클릭이벤트
+
 
 	//그외
 	void					setCurClickedItem(ClickedItem clickedItem);
 
 
 
+
 private:
-	//ComponentType		   
-	//curEquipmentesInfo     m_CurEquipmentInfo;
-	Equipmentes							m_CurBeInstalledEquipmentes;
-	Equipmentes							m_CurBeChosenEquipmentDetail;
+	AllStatus							m_CurBeInstalledEquipment;
+	AllStatus							m_ChangeValue;
+
 	EquipmentType						m_CurBeChosenEquipType = EMT_START;
 	ButtonLayer*						m_UpgradeButton = nullptr;
 	ButtonLayer*						m_EquipButton = nullptr;
 	ClickedItem							m_PrevClickedItem;
 	ClickedItem							m_CurClickedItem;
+	AllStatus							m_CurAllStatus;
 
 	std::string			   				m_StautsString[20];
 	std::vector<cocos2d::Label*>		m_BasicStatusDesc;
 	std::vector<cocos2d::Label*>		m_BasicStatusValue;
 	std::vector<cocos2d::Label*>		m_AllStatusDesc;
 	std::vector<cocos2d::Label*>		m_AllStatusValue;
+	std::vector<cocos2d::Label*>		m_ArrowLabels;	
+
+	std::map<AllstatusEnum, cocos2d::Label*> m_AllStatusChangeValue;
 
 };
