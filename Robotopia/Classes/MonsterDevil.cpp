664d2f71b5fd50c0a1055cd3c1d0fce8476e5087
abiles <pgalpga@hanmail.net>
2014-12-10 15:30:22 +0900

    Resource Type change devil

--- Robotopia/Classes/MonsterDevil.cpp
+++ Robotopia/Classes/MonsterDevil.cpp
@@ -1,5 +1,8 @@
 #include "pch.h"
 #include "MonsterDevil.h"
+#include "GameManager.h"
+#include "ComponentManager.h"
+#include "AnimationComponent.h"
 
 #define DEVIL_WIDTH 30
 #define DEVIL_HEIGHT 30
@@ -19,7 +22,7 @@ bool MonsterDevil::init()
 	m_Body = cocos2d::PhysicsBody::createBox(cocos2d::Size(DEVIL_WIDTH, DEVIL_HEIGHT), meterial, cocos2d::Point(0, 0));
 	m_Body->setContactTestBitmask(PHYC_BLOCK | PHYC_PLAYER | PHYC_MISSILE);
 	m_Body->setCategoryBitmask(PHYC_MONSTER);
-	m_Body->setCollisionBitmask(PHYC_BLOCK | PHYC_FLOOR | PHYC_MISSILE);
+	m_Body->setCollisionBitmask(PHYC_BLOCK | PHYC_MISSILE);
 	m_Body->setMass(10);
 	m_Body->setRotationEnable(false);
 	m_Body->setVelocityLimit(1000);
@@ -28,7 +31,70 @@ bool MonsterDevil::init()
 	m_Body->retain();
 	setPhysicsBody(m_Body);
 
+	initFSM(1);
+	m_States[0] = STAT_IDLE;
+
+	m_FSMs[0].resize(STAT_NUM);
+	m_FSMs[0][STAT_IDLE] = nullptr;
+	m_FSMs[0][STAT_MOVE] = FSM_CALLBACK(move, this);
+	m_FSMs[0][STAT_ATTACK] = FSM_CALLBACK(attack, this);
+
+	m_Transitions[0].resize(STAT_NUM);
+	m_Transitions[0][STAT_IDLE] = FSM_CALLBACK(idleTransition, this);
+	m_Transitions[0][STAT_MOVE] = FSM_CALLBACK(moveTransition, this);
+	m_Transitions[0][STAT_ATTACK] = FSM_CALLBACK(attackTransition, this);
+
+
+	m_Renders[0].resize(STAT_NUM);
+	m_Renders[0][STAT_MOVE] = GET_COMPONENT_MANAGER()->createComponent<AnimationComponent>();
+	((AnimationComponent*)m_Renders[0][STAT_MOVE])->setAnimation(AT_DEVIL_IDLE, this);
+	m_Renders[0][STAT_MOVE] = GET_COMPONENT_MANAGER()->createComponent<AnimationComponent>();
+	((AnimationComponent*)m_Renders[0][STAT_MOVE])->setAnimation(AT_DEVIL_MOVE, this);
+	m_Renders[0][STAT_ATTACK] = GET_COMPONENT_MANAGER()->createComponent<AnimationComponent>();
+	((AnimationComponent*)m_Renders[0][STAT_MOVE])->setAnimation(AT_DEVIL_ATTACK, this);
+
+	//for (int i = 0; i < m_Renders[0].size(); i++)
+	//{
+	//	addComponent(m_Renders[0][i]);
+	//}
+
+	////info ¼³Á¤
+	//auto data = GET_DATA_MANAGER()->getMonsterInfo(OT_MONSTER_RUSH);
+
+	//if (data != nullptr)
+	//{
+	//	m_Info = *GET_DATA_MANAGER()->getMonsterInfo(OT_MONSTER_RUSH);
+	//}
+
+	//m_Info.m_CurrentHp = m_Info.m_MaxHp;
+
+
 	return true;
+}
+
+void MonsterDevil::move()
+{
+
+}
+
+void MonsterDevil::attack()
+{
+
+}
+
+void MonsterDevil::idleTransition()
+{
+
+}
+
+void MonsterDevil::moveTransition()
+{
+
+}
+
+void MonsterDevil::attackTransition()
+{
+
 }
 
 void MonsterDevil::update(float dTime)
@@ -55,3 +121,6 @@ void MonsterDevil::exit()
 {
 
 }
+
+
+
--- Robotopia/Classes/MonsterDevil.h
+++ Robotopia/Classes/MonsterDevil.h
@@ -16,12 +16,28 @@ Comment			: DevilMonster 동작 정의.
 class MonsterDevil : public Creature
 {
 public:
+	enum State
+	{
+		STAT_IDLE,
+		STAT_MOVE,
+		STAT_ATTACK,
+		STAT_NUM,
+	};
+
 	virtual bool					init();
 	virtual void					update(float dTime);
 	virtual void					updateFSM(float dTime);
 	virtual void					updateRender(float dTime);
 	virtual void					enter();
 	virtual void					exit();
+
+	void							move();
+	void							attack();
+
+	void							idleTransition();
+	void							moveTransition();
+	void							attackTransition();
+
 
 
 private:
