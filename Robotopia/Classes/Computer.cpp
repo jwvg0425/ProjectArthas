daf841453b9d4c56d28788e9280512a4b093942d
abiles <pgalpga@hanmail.net>
2014-12-20 15:24:56 +0900

    컴퓨터랑 전기 트랩소리

--- Robotopia/Classes/Computer.cpp
+++ Robotopia/Classes/Computer.cpp
@@ -7,6 +7,7 @@
 #include "GaugeBarContainer.h"
 #include "EffectManager.h"
 #include "Effect.h"
+#include "SoundManager.h"
 
 bool Computer::init()
 {
@@ -84,6 +85,7 @@ void Computer::availableTransition(Creature* target, double dTime, int idx)
 	}
 	else if(GET_INPUT_MANAGER()->getKeyState(KC_UP) == KS_PRESS)
 	{
+		GET_SOUND_MANAGER()->createSound(SoundManager::CHARGEGAGE, false);
 		availableExit(dTime, idx);
 		loadingEnter(dTime, idx);
 		setState(idx, Computer::STAT_LOADING);
@@ -106,6 +108,7 @@ void Computer::loadingTransition(Creature* target, double dTime, int idx)
 	else if(m_IsComplete)
 	{
 		loadingExit(dTime, idx);
+		GET_SOUND_MANAGER()->createSound(SoundManager::GETMONEY, false);
 		completeEnter(dTime, idx);
 		setState(idx, Computer::STAT_COMPLETE);
 	}
